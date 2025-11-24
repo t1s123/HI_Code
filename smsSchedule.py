f56b0c4621194ab10931e2b578829e8ffd0462d1
Ryan Maina Njonjo <114038081+t1s123@users.noreply.github.com>
2025-11-24 16:50:48 -0500

    Update smsSchedule.py

--- smsSchedule.py
+++ smsSchedule.py
@@ -16,13 +16,15 @@ today = date.today()
 now=datetime.now()
 key=reader.read('TextBeeKey.txt')
 
+has="3477409462"
+nhu="2403149462"
+
 spreadsheet='https://docs.google.com/spreadsheets/d/1TNfQEpC2mWLuSg0wZZLcKmlKocfoemsnc4eREBVhg2Y/edit?gid=0#gid=0'
 
-schedule_sheet = Sheets.Sheets(spreadsheet).get_sheet_by_name('Scheduler')
+prov_ma_sheet = Sheets.Sheets(spreadsheet).get_sheet_by_name('Provider and MA')
 info = Sheets.Sheets(spreadsheet).get_sheet_by_name('Scheduler Info')
 
-headers = schedule_sheet.row_values(1)
-schedule_start_dates = schedule_sheet.col_values(headers.index("Start date") + 1)[1:]
+schedule_start_dates = prov_ma_sheet.col_values(prov_ma_sheet.row_values(1).index("Date") + 1)[1:]
 
 for each_date in schedule_start_dates:
     start_date = datetime.strptime(each_date, "%m/%d/%Y").date()
@@ -31,10 +33,14 @@ for each_date in schedule_start_dates:
         delta=(start_date-today).days
         break
 
-scheduler_name=schedule_sheet.row_values(schedule_sheet.find(start_str).row)[2]
-scheduler_phone_number=info.row_values(info.find(scheduler_name).row)[2]
+ma_1_name=prov_ma_sheet.row_values(prov_ma_sheet.find(start_str).row)[2]
+ma_2_name=prov_ma_sheet.row_values(prov_ma_sheet.find(start_str).row)[3]
 
-scheduler_number=re.sub(r'\D', '', scheduler_phone_number)  
+ma_1_number=re.sub(r'\D', '', info.row_values(info.find(ma_1_name).row)[2]).strip()
+ma_2_number=re.sub(r'\D', '', info.row_values(info.find(ma_2_name).row)[2]).strip()
+
+ma_1_name=ma_1_name.strip()
+ma_2_name=ma_2_name.strip()
 
 if(delta==0):
     schedule="today!"
@@ -43,32 +49,56 @@ elif(delta>0):
 if(delta>1):
     schedule="in "+str(delta)+" days."
 
-if(check.check(scheduler_name)):
-    if((now.hour==21 and now.minute>=45) or (now.hour==22 and now.minute<=20)):
-        message = f"Hello "+scheduler_name+"!"+" This is a reminder that you are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
-        #scheduler_number="4437937273"
-        messenger.text(scheduler_number,message,key)
-        #input("NUMBER: "+scheduler_number+"\nMESSAGE: "+message+"\nKEY: "+key)
-        #time.sleep(300)
+if((now.hour==21 and now.minute>=45) or (now.hour==22 and now.minute<=15)):
+    if(check.check(ma_1_name)):
+        ma_1_message = f"Hello "+ma_1_name+"!"+" This is a reminder that you are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
 
-else:
-    prev_name=reader.readlines('info.txt')[0]
-    prev_number=reader.readlines('info.txt')[1]
+        messenger.text(ma_1_number,ma_1_message,key)
 
-    message_current = f"Hello "+scheduler_name+"!"+" This is a reminder that you have been switched out for "+prev_name+" and are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
-    #prev_number="4437937273"
-    messenger.text(prev_number,message_current,key)
-    #input("NUMBER: "+scheduler_number+"\nMESSAGE: "+message_current+"\nKEY: "+key)
-    message_prev = f"Hello "+prev_name+"!"+" This is a you have been switched out with "+scheduler_name+" for "+schedule+", you do NOT need to come in."
-    #scheduler_number="4437937273"
-    messenger.text(scheduler_number,message_prev,key)
-    #input("NUMBER: "+scheduler_number+"\nMESSAGE: "+message_prev+"\nKEY: "+key)
+        messenger.text(has,ma_1_message,key)
 
+        messenger.text(nhu,ma_1_message,key)
+
+    if(check.check(ma_2_name)):
+        ma_2_message = f"Hello "+ma_2_name+"!"+" This is a reminder that you are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
+
+        messenger.text(ma_2_number,ma_2_message,key)
+
+        messenger.text(has,ma_2_message,key)
+
+        messenger.text(nhu,ma_2_message,key)
+
+if(not check.check(ma_1_name)):
+    prev_ma_1=reader.readlines('info.txt')[0].strip()
+    prev_ma_1_number=reader.readlines('info.txt')[1]
+
+    message_current = f"Hello "+ma_1_name+"!"+" This is a reminder that you have been switched out for "+prev_ma_1+" and are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
+    message_prev = f"Hello "+prev_ma_1+"!"+" This is a you have been switched out with "+ma_1_name+" for the shift "+schedule+" you do NOT need to come in."
+
+    messenger.text(ma_1_number,message_current,key)
+    messenger.text(prev_ma_1_number,message_prev,key)
+
+    messenger.text(has,message_current,key)
+    messenger.text(has,message_prev,key)
+
+    messenger.text(nhu,message_current,key)
+    messenger.text(nhu,message_prev,key)
+
+elif(not check.check(ma_2_name)):
+    prev_ma_2=reader.readlines('info.txt')[2].strip()
+    prev_ma_2_number=reader.readlines('info.txt')[3]
+
+    message_current = f"Hello "+ma_2_name+"!"+" This is a reminder that you have been switched out for "+prev_ma_2+" and are scheduled for "+start_str+", which is "+schedule+" Please remember to come in."
+    message_prev = f"Hello "+prev_ma_2+"!"+" This is a you have been switched out with "+ma_2_name+" for the shift "+schedule+" You do NOT need to come in."
+
+    messenger.text(ma_2_number,message_current,key)
+    messenger.text(prev_ma_2_number,message_prev,key)
+
+    messenger.text(has,message_current,key)
+    messenger.text(has,message_prev,key)
+
+    messenger.text(nhu,message_current,key)
+    messenger.text(nhu,message_prev,key)
 
 with open('info.txt','w') as f:
-    f.writelines([scheduler_name+'\n',scheduler_number+'\n',start_str+'\n'])
-
-
-
-
-
+    f.writelines([ma_1_name+'\n',ma_1_number+'\n',ma_2_name+'\n',ma_2_number+'\n',start_str+'\n'])
